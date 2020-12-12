c3bf682041c72ce16e5dde4f8947eb8826c809d7
Michal Muszynski <m.muszy@gmail.com>
2020-12-12 22:03:40 +0100

    Add translation definitions

--- src/app/translate/lang-en.ts
+++ src/app/translate/lang-en.ts
@@ -1,5 +1,11 @@
 export const LANG_EN_NAME = 'en';
 
 export const LANG_EN_TRANS = {
-  'hello world': 'hello world',
+  'place name': 'Type your phrase',
+  'search results': 'Search results',
+  'search': 'Search',
+  'search everything': 'Show all places',
+  'search endings': 'Ending with only',
+  'search beginnings': 'Starting with only',
+  'no results': 'No results',
 };
--- src/app/translate/lang-pl.ts
+++ src/app/translate/lang-pl.ts
@@ -1,6 +1,12 @@
 export const LANG_PL_NAME = 'pl';
 
 export const LANG_PL_TRANS = {
-  'hello world': 'witaj świecie',
+  'place name': 'Wpisz frazę',
+  'search results': 'Wyniki wyszukiwania',
+  'search': 'Szukaj',
+  'search everything': 'Szukaj wszystko',
+  'search endings': 'Tylko końcówki',
+  'search beginnings': 'Tylko początek',
+  'no results': 'Brak wyników',
 };
 
